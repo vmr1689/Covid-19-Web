4550d625562a7ff339693d1d8dcfa176615fa2b0
Mohanraj <mvh5cob@rbeigcn.com>
2020-04-22 12:50:39 +0530

    map data added

--- covid-app/src/app/dashboard/dashboard.component.ts
+++ covid-app/src/app/dashboard/dashboard.component.ts
@@ -20,6 +20,7 @@ import {
   Guidelines
 } from '../shared/models';
 
+import SampleMapDataJson from '.././mapdata.json';
 declare var $;
 
 @Component({
@@ -40,6 +41,7 @@ export class DashboardComponent implements OnInit, OnDestroy, AfterViewInit, Aft
   public dosList: Guidelines[] = [];
   public dontsList: Guidelines[] = [];
   public symptomsList: Guidelines[] = [];
+  public mapDataList: any[] = [];
 
   public dailySpreadList: CasesTimeSeries[] = [];
 
@@ -57,6 +59,7 @@ export class DashboardComponent implements OnInit, OnDestroy, AfterViewInit, Aft
     private guidelinesService: GuidelinesService) { }
 
   ngOnInit(): void {
+    this.getMapData();
     this.getStateDistrictData();
     this.getGuidelines();
     this.getBannerData();
@@ -68,6 +71,10 @@ export class DashboardComponent implements OnInit, OnDestroy, AfterViewInit, Aft
     $('.dataTables_filter input, .dataTables_length select').addClass('form-control');
   }
 
+  public getMapData() {
+    this.mapDataList = SampleMapDataJson;
+  }
+
   public getGuidelines() {
     this.guidelinesList = [];
     this.dosList = [];
@@ -76,7 +83,6 @@ export class DashboardComponent implements OnInit, OnDestroy, AfterViewInit, Aft
     this.guidelinesService.getAllGuidelines().subscribe((response: Guidelines[]) => {
       if (response) {
         this.guidelinesList = response;
-        debugger;
         this.symptomsList = response.filter(x => x.type === 'SYMPTOMS');
         this.dontsList = response.filter(x => x.type === "DONT'S");
         this.dosList = response.filter(x => x.type === "DO'S");
@@ -175,233 +181,58 @@ export class DashboardComponent implements OnInit, OnDestroy, AfterViewInit, Aft
     this.zone.runOutsideAngular(() => {
       am4core.useTheme(am4themes_animated);
 
-      //World map
-      const countries = Countries;
-      const continents = Continents;
-
-
-      var ranges = [
-        {
-          label: '1-272',
-          color: '#rgb(255, 245, 240)',
-          percent: 30,
-          data: [{
-            state: 'Maharashtra',
-            statecode: 'MH'
-          }]
-        },
-        {
-          label: '273-544',
-          color: '#rgb(253, 213, 195)',
-          percent: 40,
-          data: [{
-            state: '',
-            statecode: ''
-          }]
-        },
-        {
-          label: '545-816',
-          color: '#rgb(252, 165, 136)',
-          percent: 50,
-          data: [{
-            state: '',
-            statecode: ''
-          }]
-        },
-        {
-          label: '817-1088',
-          color: '#rgb(250, 113, 82)',
-          percent: 60,
-          data: [{
-            state: '',
-            statecode: ''
-          }]
-        },
-        {
-          label: '1089-1360',
-          color: '#rgb(233, 57, 45)',
-          percent: 70,
-          data: [{
-            state: '',
-            statecode: ''
-          }]
-        },
-        {
-          label: '1360+',
-          color: '#rgb(189, 22, 26)',
-          percent: 80,
-          data: [{
-            state: '',
-            statecode: ''
-          }]
-        }
-      ];
-
-
-
       const mapchart = am4core.create('chartdiv', am4maps.MapChart);
       mapchart.responsive.enabled = true;
       mapchart.projection = new am4maps.projections.Miller();
 
+      const polygonSeries = mapchart.series.push(new am4maps.MapPolygonSeries());
+      polygonSeries.useGeodata = true;
+      polygonSeries.geodata = am4geodata_indiaLow;
 
-      // Create map polygon series for world map
-      const indiaSeries = mapchart.series.push(new am4maps.MapPolygonSeries());
-      indiaSeries.useGeodata = true;
-      indiaSeries.geodata = am4geodata_indiaLow;
-      //worldSeries.exclude = ["AQ"];
 
       // Configure series
-      let polygonTemplate = indiaSeries.mapPolygons.template;
-      polygonTemplate.tooltipText = "{name}";
-      polygonTemplate.fill = am4core.color("#74B266");
+      const polygonTemplate = polygonSeries.mapPolygons.template;
+      polygonTemplate.tooltipText = '{name}:{value}';
+      polygonTemplate.fill = am4core.color('#999');
 
       // Create hover state and set alternative fill color
-      let hs = polygonTemplate.states.create("hover");
-      hs.properties.fill = am4core.color("#367B25");
+      const hs = polygonTemplate.states.create('hover');
+      hs.properties.fill = am4core.color('#367B25');
 
-      // worldSeries.data = [{
-      //   "id": "US",
-      //   "name": "United States",
-      //   "value": 100,
-      //   "fill": am4core.color("#F05C5C")
-      // }, {
-      //   "id": "FR",
-      //   "name": "France",
-      //   "value": 50,
-      //   "fill": am4core.color("#5C5CFF")
-      // }];
-      // polygonTemplate.propertyFields.fill = "fill";
-      // const worldPolygon = worldSeries.mapPolygons.template;
-      // worldPolygon.tooltipText = "Region: {name}" + "\n" + "Active: {active}";
-      // worldPolygon.nonScalingStroke = true;
-      // worldPolygon.strokeOpacity = 0.5;
-      // worldPolygon.fill = am4core.color("#eee");
-      // worldPolygon.propertyFields.fill = "color";
-
-      // let hs1 = worldPolygon.states.create("hover");
-      // hs1.properties.fill = mapchart.colors.getIndex(9);
-
-      // // Create country specific series (but hide it for now)
-      // let countrySeries = mapchart.series.push(new am4maps.MapPolygonSeries());
-      // countrySeries.useGeodata = true;
-      // countrySeries.hide();
-      // countrySeries.geodataSource.events.on("done", function(ev) {
-      //   //worldSeries.hide();
-      //   countrySeries.show();
-      // });
-
-      // let countryPolygon = countrySeries.mapPolygons.template;
-      // countryPolygon.tooltipText = "Region: {name}";
-      // countryPolygon.nonScalingStroke = true;
-      // countryPolygon.strokeOpacity = 0.5;
-      // countryPolygon.fill = am4core.color("#eee");
-
-      // let hs2 = countryPolygon.states.create("hover");
-      // hs2.properties.fill = mapchart.colors.getIndex(9);
-
-      // // Set up click events
-      // worldPolygon.events.on("hit", function(ev) {
-      //   ev.target.series.chart.zoomToMapObject(ev.target);
-      //   let map = ev.target.dataItem.dataContext["map"];
-
-      //   if (map) {
-      //     ev.target.isHover = false;
-      //     countrySeries.geodataSource.url =
-      //       "https://www.amcharts.com/lib/4/geodata/json/" + map + ".json";
-      //     countrySeries.geodataSource.load();
-      //   }
-      // });
-
-      // //Set up data for countries
-      // let mapdata = [];
-      // for (var id in countries) {
-      //   if (countries.hasOwnProperty(id)) {
-      //     let country = countries[id];
-      //     if (country.maps.length) {
-      //       mapdata.push({
-      //         id: id,
-      //         color: mapchart.colors.getIndex(
-      //           continents[country.continent_code]
-      //         ),
-      //         map: country.maps[0]
-      //       });
-      //     }
-      //   }
-      // }
-      // worldSeries.data = mapdata;
-
-      // Zoom control
-      mapchart.zoomControl = new am4maps.ZoomControl();
-
-      let homeButton = new am4core.Button();
-      homeButton.events.on("hit", function () {
-        // worldSeries.show();
-        // countrySeries.hide();
-        mapchart.goHome();
+      // Add heat rule
+      polygonSeries.heatRules.push({
+        property: 'fill',
+        target: polygonSeries.mapPolygons.template,
+        min: am4core.color('#ffcccc'),
+        max: am4core.color('#990000')
       });
 
-      homeButton.icon = new am4core.Sprite();
-      homeButton.padding(7, 5, 7, 5);
-      homeButton.width = 30;
-      homeButton.icon.path =
-        "M16,8 L14,8 L14,16 L10,16 L10,10 L6,10 L6,16 L2,16 L2,8 L0,8 L8,0 L16,8 Z M16,8";
-      homeButton.marginBottom = 10;
-      homeButton.parent = mapchart.zoomControl;
-      homeButton.insertBefore(mapchart.zoomControl.plusButton);
+      const heatLegend = mapchart.createChild(am4maps.HeatLegend);
+      heatLegend.id = 'heatLegend';
+      heatLegend.series = polygonSeries;
+      heatLegend.align = 'right';
+      heatLegend.valign = 'bottom';
+      heatLegend.width = am4core.percent(50);
+      heatLegend.marginRight = am4core.percent(4);
+      heatLegend.background.fill = am4core.color('#000');
+      heatLegend.background.fillOpacity = 0.05;
+      heatLegend.padding(5, 5, 5, 5);
 
-      // //create capital markers
-      // let imageSeries = mapchart.series.push(new am4maps.MapImageSeries());
-      // let targetSVG =
-      //   "M9,0C4.029,0,0,4.029,0,9s4.029,9,9,9s9-4.029,9-9S13.971,0,9,0z M9,15.93 c-3.83,0-6.93-3.1-6.93-6.93S5.17,2.07,9,2.07s6.93,3.1,6.93,6.93S12.83,15.93,9,15.93 M12.5,9c0,1.933-1.567,3.5-3.5,3.5S5.5,10.933,5.5,9S7.067,5.5,9,5.5 S12.5,7.067,12.5,9z";
-      // // define template
-      // let imageSeriesTemplate = imageSeries.mapImages.template;
-      // let circle = imageSeriesTemplate.createChild(am4core.Sprite);
-      // circle.scale = 0.4;
-      // circle.fill = new am4core.InterfaceColorSet().getFor(
-      //   "alternativeBackground"
-      // );
-      // circle.path = targetSVG;
-      // // what about scale...
+      polygonSeries.data = this.mapDataList;
 
-      // // set propertyfields
-      // imageSeriesTemplate.propertyFields.latitude = "latitude";
-      // imageSeriesTemplate.propertyFields.longitude = "longitude";
+      polygonSeries.mapPolygons.template.events.on('over', (ev) => {
+        if (!isNaN(ev.target.dataItem.value)) {
+          heatLegend.valueAxis.showTooltipAt(ev.target.dataItem.value);
+        }
+        else {
+          heatLegend.valueAxis.hideTooltip();
+        }
+      });
 
-      // imageSeriesTemplate.horizontalCenter = "middle";
-      // imageSeriesTemplate.verticalCenter = "middle";
-      // imageSeriesTemplate.align = "center";
-      // imageSeriesTemplate.valign = "middle";
-      // imageSeriesTemplate.width = 8;
-      // imageSeriesTemplate.height = 8;
-      // imageSeriesTemplate.nonScaling = true;
-      // imageSeriesTemplate.tooltipText =
-      //   "Region: {title}" +
-      //   "\n" +
-      //   "Active: {active}" +
-      //   "\n" +
-      //   "In-active: {inactive}" +
-      //   "\n" +
-      //   "Healthy: {healthy}";
-      // imageSeriesTemplate.fill = am4core.color("#000");
-      // imageSeriesTemplate.background.fillOpacity = 0;
-      // imageSeriesTemplate.background.fill = am4core.color("#ffffff");
-      // imageSeriesTemplate.setStateOnChildren = true;
-      // imageSeriesTemplate.states.create("hover");
-      // imageSeriesTemplate.events.on("hit", ev => {
-      //   console.log(ev.target.dataItem.dataContext);
+      polygonSeries.mapPolygons.template.events.on('out', (ev) => {
+        heatLegend.valueAxis.hideTooltip();
+      });
 
-      //   debugger;
-      //   ev.target.series.chart.zoomToMapObject(ev.target);
-
-      //   let map = ev.target.dataItem.dataContext["map"];
-
-      //   if (map) {
-      //     ev.target.isHover = false;
-      //     countrySeries.geodataSource.url =
-      //       "https://www.amcharts.com/lib/4/geodata/json/" + map + ".json";
-      //     countrySeries.geodataSource.load();
-      //   }
-      // });
       this.mapchart = mapchart;
       this.renderDailyCharts();
     });
@@ -469,7 +300,7 @@ export class DashboardComponent implements OnInit, OnDestroy, AfterViewInit, Aft
     categoryAxis.renderer.labels.template.rotation = -90;
     categoryAxis.renderer.labels.template.horizontalCenter = 'left';
     categoryAxis.renderer.labels.template.location = 0.5;
-    categoryAxis.renderer.labels.template.adapter.add('dx', function (dx, target) {
+    categoryAxis.renderer.labels.template.adapter.add('dx', (dx, target) => {
       return -target.maxRight / 2;
     });
     const valueAxis = confirmedChart.yAxes.push(new am4charts.ValueAxis());
@@ -512,7 +343,7 @@ export class DashboardComponent implements OnInit, OnDestroy, AfterViewInit, Aft
     recoveredCategoryAxis.renderer.labels.template.rotation = -90;
     recoveredCategoryAxis.renderer.labels.template.horizontalCenter = 'left';
     recoveredCategoryAxis.renderer.labels.template.location = 0.5;
-    recoveredCategoryAxis.renderer.labels.template.adapter.add("dx", function (dx, target) {
+    recoveredCategoryAxis.renderer.labels.template.adapter.add('dx', (dx, target) => {
       return -target.maxRight / 2;
     });
     const recoveredvalueAxis = recoveredChart.yAxes.push(new am4charts.ValueAxis());
@@ -555,7 +386,7 @@ export class DashboardComponent implements OnInit, OnDestroy, AfterViewInit, Aft
     deceasedCategoryAxis.renderer.labels.template.rotation = -90;
     deceasedCategoryAxis.renderer.labels.template.horizontalCenter = 'left';
     deceasedCategoryAxis.renderer.labels.template.location = 0.5;
-    deceasedCategoryAxis.renderer.labels.template.adapter.add('dx', function (dx, target) {
+    deceasedCategoryAxis.renderer.labels.template.adapter.add('dx', (dx, target) => {
       return -target.maxRight / 2;
     });
     const deceasedvalueAxis = deceasedChart.yAxes.push(new am4charts.ValueAxis());
@@ -575,7 +406,7 @@ export class DashboardComponent implements OnInit, OnDestroy, AfterViewInit, Aft
     deceasedseries.tooltip.pointerOrientation = 'horizontal';
     deceasedseries.bullets.create(am4charts.CircleBullet);
     deceasedChart.cursor = new am4charts.XYCursor();
-    
+
     this.deceasedChart = deceasedChart;
   }
 
@@ -600,7 +431,7 @@ export class DashboardComponent implements OnInit, OnDestroy, AfterViewInit, Aft
     categoryAxis.renderer.grid.template.location = 0;
     categoryAxis.renderer.minGridDistance = 30;
 
-    categoryAxis.renderer.labels.template.adapter.add('dy', function (dy, target) {
+    categoryAxis.renderer.labels.template.adapter.add('dy', (dy, target) => {
       // if (target.dataItem && (target.dataItem.index & 2) == 2) {
       //   return dy + 25;
       // }
@@ -654,7 +485,6 @@ export class DashboardComponent implements OnInit, OnDestroy, AfterViewInit, Aft
 
     pieSeries.labels.template.disabled = true;
     pieSeries.ticks.template.disabled = true;
-    //pieSeries.slices.template.tooltipText = "";
 
     const as = pieSeries.slices.template.states.getKey('active');
     as.properties.shiftRadius = 0;
